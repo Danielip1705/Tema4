5e7cb06be58e6ca714b13818bbe27df287b241a0
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-01-10 18:14:16 +0100

    Array

--- src/array/Ej8.java
+++ src/array/Ej8.java
@@ -16,14 +16,16 @@ public class Ej8 {
 		
 		//creamos random
 		Random rand = new Random();
-		
+		//creamos array para que se inserte un valor aleatorio en cada indice
 		for(int i=0;i<lista.length;i++) {
+			//inicializamos cada indice a un valor aleatorio entre el 1 y el 10
 			lista[i]= rand.nextInt(11);
 		}
+		//imprimimos en consola que inserte un numero entre el 1 y el 100
 		System.out.println("Inserte un valor del 1 al 100 para saber su resultado obtenido");
-		
+		//escribimos el numero
 		num = sc.nextInt();
-		
+		//imprimimos resultado
 		System.out.println(lista[num]);
 		
 		
