34e37c62db7a1a46218eb9b862b5dcb05eee158e
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-01-11 13:42:29 +0100

    Array

--- src/array/Ej7.java
+++ src/array/Ej7.java
@@ -6,26 +6,30 @@ public class Ej7 {
 
 	public static void main(String[] args) {
 		//Creamos un array con la variable int y con logitud
-		int meses[]=new int[12];
+		int temperatura[]=new int[12];
+		
+		String meses[] = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
 		
 		//creamos scanner
 		Scanner sc = new Scanner(System.in);
 		
 		//creamos bucle for para quue insertemos los datos en el array
-		for(int i=0;i<meses.length;i++) {
+		for(int i=0;i<temperatura.length;i++) {
 			//imprimos en pantalla que nos inserte la tempertura
-			System.out.println("Inserte la temperatura");
+			System.out.println("Inserte la temperatura del mes de "+meses[i]);
 			
 			//Insertamos el dato en el array
-			meses[i]=sc.nextInt();
+			temperatura[i]=sc.nextInt();
 			
 		}
 		//creamos bucle for para que lea el array en su totalidad
-		for(int i=0;i<meses.length;i++) {
+		for(int i=0;i<temperatura.length;i++) {
+			System.out.println(meses[i]);
+			System.out.println(temperatura[i]+"º");
 			//creamos for anidado para que nos imprima una bara doble
 			for(int j=1;j<=2;j++) {
 				//creamos otro for anidado para que nos imprima "*" segun el numero que tenga
-				for(int n=1;n<=meses[i];n++) {
+				for(int n=1;n<=temperatura[i];n++) {
 					//imprimimos en consola *
 					System.out.print("*");
 				}
---
+++ src/array1/Ej1.java
@@ -0,0 +1,16 @@
+package array1;
+
+import java.util.Scanner;
+
+public class Ej1 {
+
+	public static void main(String[] args) {
+		//creamos con la variable int el tamaño y el relleno
+		int relleno,tamaño;
+		
+		int lista[];
+		
+		Scanner sc = new Scanner(System.in);
+	}
+
+}
