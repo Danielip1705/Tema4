283cd0023b0213abcd2aa1b82ec471cefd69d45b
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-01-17 11:49:58 +0100

    Array

--- src/array2/Ej3.java
+++ src/array2/Ej3.java
@@ -14,7 +14,7 @@ public class Ej3 {
 
 		// Creamos array con la variable int
 		int lista[];
-		
+
 		int resultado[];
 
 		// creamos scanner
@@ -31,23 +31,37 @@ public class Ej3 {
 
 		// imprimimos en consola que nos indique el fin del relleno
 		System.out.println("Indica que numero del relleno es el limite");
-		
+
+		// escribimos fin
 		fin = sc.nextInt();
-		
+
+		// imprimimos en pantalla con la clase array.toString el resultado de la funcion
+		// rellenaPares
 		System.out.println(Arrays.toString(Ej3.rellenaPares(longitud, fin)));
 	}
 
+	// creamos funcion que devuelva un array entero y con 2 parametros de entrada
 	public static int[] rellenaPares(int longitud, int fin) {
-		
+
+		// creamos array con la variable int y su longitud sera el resultado del
+		// parametro longitud
 		int j[] = new int[longitud];
 
+		// creamos bucle for que recorra todo el array para añadirle los datos
 		for (int i = 0; i < j.length; i++) {
 
+			// rellenamos cada dato con un numero aleatorio entre el 2 y fin
+			j[i] = (int) (Math.random() * (fin-1) + 2);
+
+			// creamos bucle while haciendo que si el dato no es par, que le de otro numero
+			// aleatorio entre el 2 y el fin. Hasta que sea par
 			while (j[i] % 2 != 0) {
+
+				// rellenamos cada dato con un numero aleatorio entre el 2 y fin
 				j[i] = (int) (Math.random() * fin + 2);
 			}
 		}
-
+		// devolvemos el array j
 		return j;
 	}
 
---
+++ src/array2/Ej4.java
@@ -0,0 +1,13 @@
+package array2;
+
+public class Ej4 {
+
+	public static void main(String[] args) {
+		//creamos con la variable int un array con longitud 50
+		int t[] = new int[50];
+		
+		int clave;
+
+	}
+
+}
