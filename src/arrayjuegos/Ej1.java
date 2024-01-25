0c8b52c44be68379acc3e2c63ae26a5937880f06
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-01-25 14:38:28 +0100

    array

--- src/arrayjuegos/Ej1.java
+++ src/arrayjuegos/Ej1.java
@@ -7,27 +7,30 @@ import java.util.Scanner;
 public class Ej1 {
 
 	public static void main(String[] args) {
-		//Creamos un array con la variable int y de longitud 10
-		char vector[] = new char [10];
-		
-		int posicion[]=new int [10];
-		
-		//Con la variable string creamos mosca y lo inicializamos a "X"
-		String mosca = "X";
-		
-		int golpear;
-		
-		Random rand = new Random(); 
-		
-		Scanner sc = new Scanner(System.in);
-		
-		golpear = rand.nextInt(vector.length);
-		
-		System.out.println(Arrays.toString(vector));
-		
-		System.out.println("Indica");
-		
-		
-	}
+		// Creamos un array con la variable int y de longitud 10
+		char vector[] = new char[10];
 
+		int posicion[] = new int[10];
+
+		// Con la variable string creamos mosca y lo inicializamos a "X"
+		int mosca, golpear, indice = 0;
+
+		Random rand = new Random();
+
+		Scanner sc = new Scanner(System.in);
+
+		mosca = rand.nextInt(vector.length);
+
+		posicion[mosca]=1;
+		
+		
+
+		
+			
+
+		
+		
+	
+
+	}
 }
