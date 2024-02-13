a6a385f0b0571914f4cf060851c9b3dc82b7afb5
Danielip1705 <daniel.iglesias@iesnervion.es>
2024-02-13 14:34:58 +0100

    String

--- src/strings/Ej5.java
+++ src/strings/Ej5.java
@@ -3,19 +3,30 @@ package strings;
 public class Ej5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creamos con la clase String frase e inicializamos una cadena de texto
 		String frase = "Probando frase";
 		
+		//Creamos con la clase String invertido que tendra inicializado el resultado de la funcion invertido
+		String resultado = invertido(frase);
+		
+		//Imprimimos la variable resultado
+		System.out.println(resultado);
 		
 	}
 	
-	public static String(String frase) {
+	//Creamos una funcion que tenga como parametro de entrada una cadena y que devuelva otra cadena invertida
+	public static String invertido(String frase) {
 		
-		String invertido="";
+		//Creamos con la clase String cadenaInvertida y que tenga inicializado una cadena vacia
+		String cadenaInvertida="";
 		
-		
-		
-		return invertido;
+		//Creamos bucle for que lea la cadena frase invertida (de fin a principio)
+		for(int i=frase.length()-1;i>=0;i--) {
+			
+			//Suma
+			cadenaInvertida+=frase.charAt(i);
+		}
+		return cadenaInvertida;
 	}
 
 }
